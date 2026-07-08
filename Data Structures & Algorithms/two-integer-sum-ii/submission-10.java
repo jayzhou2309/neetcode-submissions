80bef8e887826807ac03f136abf715528d52c5ed
Jay Zhou <spammyjunbai@gmail.com>
2026-07-08 17:42:41 +0800

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++){
+            int x = target - numbers[i];
+            for (int j = i + 1; j < numbers.length; j++){
+                if (x == numbers[j]){
+                    return new int [] {i + 1, j + 1};
+                }
+            }
+        }
+
+        return new int [] {};
+    }
+}
