6e05621fa1ccade51909dabd747d9614d01a8397
Jay Zhou <spammyjunbai@gmail.com>
2026-07-09 10:20:02 +0800

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs){
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String sortedS = new String(ca);
+            // if doesnt exist
+            map.putIfAbsent(sortedS, new ArrayList<>());
+            // if exist
+            map.get(sortedS).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
