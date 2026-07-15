8829816dc0661c488e4be17a435f11bb906f1bdf
Jay Zhou <spammyjunbai@gmail.com>
2026-07-15 17:27:01 +0800

    Add: search-2d-matrix - submission-16

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-16.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int ROWS = arr.length;
+        int COLS = arr[0].length;
+
+        int l = 0, r = ROWS * COLS - 1;
+
+        while (l <= r){
+            int m = l + (r - l) / 2;
+
+            int row = m / COLS;
+            int col = m % COLS;
+
+            if(arr[row][col] < target){
+                l = m + 1;
+            } else if (arr[row][col] > target){
+                r = m - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
