193c8774d0f4207f39fdf6948694f65338cd5aed
Jay Zhou <spammyjunbai@gmail.com>
2026-07-29 13:37:22 +0800

    Update: three-integer-sum - submission-6

--- Data Structures & Algorithms/three-integer-sum/submission-6.java
+++ Data Structures & Algorithms/three-integer-sum/submission-6.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++){
-            // i = anchor
-            // everything >> is positive, cant add to 0;
+        for (int i = 0; i < n; i++){
             if (nums[i] > 0) break;
-            // sorted array, skip previous same element;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1, right = nums.length - 1;
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
-                    left++;
+            int l = i + 1, r = n - 1;
+            while (l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum < 0) {
+                    l++;
+                } else if (sum > 0){
+                    r--;
                 } else {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
 
-                    while (left < right && nums[left] == nums[left - 1]){
-                        left++;
+                    while (l < r && nums[l] == nums[l - 1]){
+                        l++;
                     }
                 }
             }
         }
         return res;
-
     }
 }
