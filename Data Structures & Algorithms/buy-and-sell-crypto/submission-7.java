e6100fff09db6d0c70db2505d33782d50980299a
Jay Zhou <spammyjunbai@gmail.com>
2026-07-29 15:36:25 +0800

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int l = 0;
+        int r = l + 1;
+
+        while (r < prices.length){
+            if (prices[r] > prices[l]){
+                int p = prices[r] - prices[l];
+                res = Math.max(res, p);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return res;
+    }
+}
