b2a1baffc8e115814288a84c02543d60efaa8192
Jay Zhou <spammyjunbai@gmail.com>
2026-08-04 03:30:13 +0800

    Update: is-palindrome - submission-4

--- Data Structures & Algorithms/is-palindrome/submission-4.java
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int left = 0;
-        int right = cleaned.length() - 1;
-        while(left < right){
-            if (cleaned.charAt(left) != cleaned.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+        int l = 0;
+        int r = cleaned.length() - 1;
+
+        while (l <= r){
+            if (cleaned.charAt(l) != cleaned.charAt(r)) return false;
+            l++;
+            r--;
         }
+        
         return true;
+
     }
 }
