667acca15aa7f8f8ae66f9be6115aeb6c5dccc53
Jay Zhou <zhoujunbai123@gmail.com>
2026-08-15 23:36:40 +0800

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        bt(nums, 0, res, path);
+        return res;
+    }
+
+    private void bt (int[] nums,int start, List<List<Integer>> res, List<Integer> path){
+        res.add(new ArrayList<>(path));
+        for (int i = start; i < nums.length; i++){
+            path.add(nums[i]);
+            bt(nums, i + 1, res, path);
+            path.remove(path.size() - 1);
+        }
+        
+    }
+}
