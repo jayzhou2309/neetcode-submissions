9f8002c5a414c72f2a787809f2b3fb1c6640affe
Jay Zhou <zhoujunbai123@gmail.com>
2026-08-16 23:41:32 +0800

    Add: permutations - submission-7

---
+++ Data Structures & Algorithms/permutations/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        bt(nums, used, res, new ArrayList<>());
+        return res;
+    }
+
+    private void bt(int[] nums, boolean[] used, List<List<Integer>> res, List<Integer> path){
+        if (path.size() == nums.length){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++){
+            if (used[i]) continue;
+            
+            used[i] = true;
+            path.add(nums[i]);
+            bt(nums, used, res, path);
+            used[i] = false;
+            path.remove(path.size() -1);
+        }
+    }
+}
