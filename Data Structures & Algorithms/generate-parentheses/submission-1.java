f23d769f1b0a0ce435e9ab6ec28f66bb55db4a10
Jay Zhou <zhoujunbai123@gmail.com>
2026-08-17 00:09:31 +0800

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        bt(res, "", 0, 0, n);
+        return res;
+        
+    }
+
+    private void bt(List<String> res, String s, int open, int close, int n){
+        if (s.length() == 2 * n){
+            res.add(s);
+            return;
+        }
+
+        if (open < n){
+            bt(res, s + '(', open + 1, close, n);
+        }
+
+        if (close < open){
+            bt(res, s + ')', open, close + 1, n);
+        }
+    }
+}
