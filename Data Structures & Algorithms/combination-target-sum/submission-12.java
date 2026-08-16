5656bc30f670e27706a02b8a89f5d27e9a497c31
Jay Zhou <zhoujunbai123@gmail.com>
2026-08-16 23:30:57 +0800

    Add: combination-target-sum - submission-12

---
+++ Data Structures & Algorithms/combination-target-sum/submission-12.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        bt(nums, target, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void bt(int[] nums, int target, int start, List<List<Integer>> res, List<Integer> path){
+            if (target == 0) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+        for (int i = start; i < nums.length; i++){
+            if (target < nums[i]) continue;
+            path.add(nums[i]);
+            bt(nums, target - nums[i], i, res, path);
+            path.remove(path.size() - 1);
+        }
+    }
+}
