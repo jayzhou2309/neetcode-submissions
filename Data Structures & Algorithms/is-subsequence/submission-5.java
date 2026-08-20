7c0f6539118ff676d09710f1b4187a9faeeba27b
Jay Zhou <zhoujunbai123@gmail.com>
2026-08-21 05:38:37 +0800

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        if (t.length() == 0) return false;
+        if (t.length() < s.length()) return false;
+
+        int s1 = 0;
+
+        for (int i = 0; i < t.length(); i++){
+            if (t.charAt(i) == s.charAt(s1)) {
+                s1++;
+
+                if (s1 == s.length()) return true;
+            }
+        }
+
+        return false;
+    }
+}
