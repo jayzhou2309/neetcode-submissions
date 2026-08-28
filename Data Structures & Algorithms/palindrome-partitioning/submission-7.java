bc8c5e100d8dc76968711dc8a5fe951539165394
Jay Zhou <zhoujunbai123@gmail.com>
2026-08-28 21:49:04 +0800

    Add: palindrome-partitioning - submission-7

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        bt(s, res,  new ArrayList<>(), 0);
+        return res;
+    }
+
+    private void bt(String s, List<List<String>> res, List<String> path, int start){
+        if (s.length() == start){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++){
+            if (isPalindrome(s, start, i)){
+                path.add(s.substring(start, i + 1));
+                bt(s, res, path, i + 1);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int l, int r){
+        while (l < r){
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
